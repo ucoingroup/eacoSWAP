fcb714bb654697ecd2e8739d8d37137b1d662265
u <zhieaco@gmail.com>
2026-07-18 08:16:12 +0800

    chore: add eacoSWAP src files

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { LanguageProvider } from './contexts/LanguageContext.tsx'
+import { WalletProvider } from './contexts/WalletContext.tsx'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <LanguageProvider>
+      <WalletProvider>
+        <App />
+      </WalletProvider>
+    </LanguageProvider>
+  </React.StrictMode>,
+)
